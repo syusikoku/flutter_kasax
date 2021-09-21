cf7e22f011f1f09aa79bed7842ac79f940a0bf43
syusikoku <xfgczzg@163.com>
2021-09-21 23:16:50 +0800

    toast更新

--- lib/kasax.dart
+++ lib/kasax.dart
@@ -5,18 +5,21 @@ import 'package:flutter/services.dart';
 class Kasax {
   static const MethodChannel _channel = const MethodChannel('kasax');
 
+  /// 测试使用
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  /// 显示系统默认 short时长 toast
+  static Future<int> showShortToast() async {
+    final int result = await _channel.invokeMethod('showShortToast');
+    return result;
   }
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  /// 显示系统默认 long时长 toast
+  static Future<int> showLongToast() async {
+    final int result = await _channel.invokeMethod('showLongToast');
+    return result;
   }
 }
